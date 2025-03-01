be0d3eccd3ecc525b0eb0dbe157ea272f00e16ab
Marat Dospolov <marat.dospolov@gmail.com>
2025-03-01 16:09:33 +0100

    Hot Module Reloading fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const inDevelopment = process.env.NODE_ENV === 'development';
+
+const nextConfig = {
+  // Set assetPrefix to our public URL
+  // Checking for inDevelopment as '/' breaks Hot Module Reloading if used during development
+  assetPrefix: inDevelopment ? undefined : '/',
+};
 
 export default nextConfig;
