f1c94abbf52e567a877dae389f1178a8ee295e75
Marat Dospolov <marat.dospolov@gmail.com>
2025-03-04 18:00:24 +0100

    Revert "cita base"
    This reverts commit f672f5f4dc9c1f3135786f3b1b6b876c9392a7cd.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="min-h-screen">
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
