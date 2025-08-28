859a78efdc8bdc8b13e722cba8bb916897da9459
Marat Dospolov <marat.dospolov@gmail.com>
2025-08-28 17:30:38 +0200

    veil bg

--- src/app/(base-layout)/PageLayout.tsx
+++ src/app/(base-layout)/PageLayout.tsx
@@ -11,7 +11,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
-import { ThemeSwitcher } from '@/components/ThemeSwitcher';
+// import { ThemeSwitcher } from '@/components/ThemeSwitcher';
 
 type PageRecord = {
   title: string;
@@ -46,103 +46,105 @@ export default function PageLayout({
   showTitle?: boolean;
 }) {
   return (
-    <div className="px-4 md:px-0">
-      <div className="container mx-auto prose dark:prose-invert pt-4 flex justify-between items-center">
-        <div className="not-prose">
-          {page.slug !== 'homepage' && (
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>{page.title}</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          )}
+    <>
+      <div className="px-4 md:px-0">
+        <div className="container mx-auto prose dark:prose-invert pt-4 flex justify-between items-center">
+          <div className="not-prose">
+            {page.slug !== 'homepage' && (
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>{page.title}</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            )}
+          </div>
+          {/* <ThemeSwitcher /> */}
         </div>
-        <ThemeSwitcher />
+        <div className="container mx-auto prose dark:prose-invert md:py-10 prose-hr:border-gray-500">
+          {showTitle && <h1>{page.title}</h1>}
+          {/*
+           * Structured Text is a JSON format similar to HTML, but with the advantage
+           * of a significantly reduced and tailored set of possible tags
+           * for editorial content, along with the capability to create hyperlinks
+           * to other DatoCMS records and embed custom DatoCMS blocks.
+           */}
+          <StructuredText
+            data={page.structuredText}
+            renderBlock={
+              /*
+               * If the structured text embeds any blocks, it's up to you to decide
+               * how to render them:
+               */
+              ({ record }) => {
+                switch (record.__typename) {
+                  case 'VideoBlockRecord': {
+                    return <VideoBlock data={record} />;
+                  }
+                  case 'ImageBlockRecord': {
+                    return <ImageBlock data={record} />;
+                  }
+                  case 'ImageGalleryBlockRecord': {
+                    return <ImageGalleryBlock data={record} />;
+                  }
+                  default: {
+                    return null;
+                  }
+                }
+              }
+            }
+            renderInlineRecord={
+              /*
+               * If the structured text includes a reference to another DatoCMS
+               * record, it's up to you to decide how to render them:
+               */
+              ({ record }) => {
+                switch (record.__typename) {
+                  case 'PageRecord': {
+                    return (
+                      <Link href={`/${record.slug}`} className="pill">
+                        {record.title}
+                      </Link>
+                    );
+                  }
+                  default: {
+                    return null;
+                  }
+                }
+              }
+            }
+            renderLinkToRecord={
+              /*
+               * If the structured text includes a link to another DatoCMS record,
+               * it's your decision to determine where the link should lead, or if
+               * you wish to customize its appearance:
+               */
+              ({ transformedMeta, record, children }) => {
+                switch (record.__typename) {
+                  case 'PageRecord': {
+                    return (
+                      <Link {...transformedMeta} href={`/${record.slug}`}>
+                        {children}
+                      </Link>
+                    );
+                  }
+                  default: {
+                    return null;
+                  }
+                }
+              }
+            }
+          />
+          <footer className="text-center text-sm pt-25 pb-10">
+            Dospolov.com &copy; {new Date().getFullYear()}
+          </footer>
+        </div>
       </div>
-      <div className="container mx-auto prose dark:prose-invert md:py-10 prose-hr:border-gray-500">
-        {showTitle && <h1>{page.title}</h1>}
-        {/*
-         * Structured Text is a JSON format similar to HTML, but with the advantage
-         * of a significantly reduced and tailored set of possible tags
-         * for editorial content, along with the capability to create hyperlinks
-         * to other DatoCMS records and embed custom DatoCMS blocks.
-         */}
-        <StructuredText
-          data={page.structuredText}
-          renderBlock={
-            /*
-             * If the structured text embeds any blocks, it's up to you to decide
-             * how to render them:
-             */
-            ({ record }) => {
-              switch (record.__typename) {
-                case 'VideoBlockRecord': {
-                  return <VideoBlock data={record} />;
-                }
-                case 'ImageBlockRecord': {
-                  return <ImageBlock data={record} />;
-                }
-                case 'ImageGalleryBlockRecord': {
-                  return <ImageGalleryBlock data={record} />;
-                }
-                default: {
-                  return null;
-                }
-              }
-            }
-          }
-          renderInlineRecord={
-            /*
-             * If the structured text includes a reference to another DatoCMS
-             * record, it's up to you to decide how to render them:
-             */
-            ({ record }) => {
-              switch (record.__typename) {
-                case 'PageRecord': {
-                  return (
-                    <Link href={`/${record.slug}`} className="pill">
-                      {record.title}
-                    </Link>
-                  );
-                }
-                default: {
-                  return null;
-                }
-              }
-            }
-          }
-          renderLinkToRecord={
-            /*
-             * If the structured text includes a link to another DatoCMS record,
-             * it's your decision to determine where the link should lead, or if
-             * you wish to customize its appearance:
-             */
-            ({ transformedMeta, record, children }) => {
-              switch (record.__typename) {
-                case 'PageRecord': {
-                  return (
-                    <Link {...transformedMeta} href={`/${record.slug}`}>
-                      {children}
-                    </Link>
-                  );
-                }
-                default: {
-                  return null;
-                }
-              }
-            }
-          }
-        />
-        <footer className="text-center text-sm pt-25 pb-10">
-          Dospolov.com &copy; {new Date().getFullYear()}
-        </footer>
-      </div>
-    </div>
+    </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
+import { DarkVeilBackground } from '@/components/ui/shadcn-io/dark-veil-background';
 
 import './global.css';
 
@@ -12,13 +13,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main>{children}</main>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          <div className="h-full fixed w-full z-0">
+            <DarkVeilBackground
+              hueShift={45}
+              scanlineIntensity={0.1}
+              scanlineFrequency={2.0}
+              noiseIntensity={0.05}
+              warpAmount={1.0}
+            />
+          </div>
+          <main className="relative z-10 min-h-screen">{children}</main>
         </ThemeProvider>
         <SpeedInsights />
         <Analytics />
